fc153f481cc50b4e6e7847a78b2f37aabfe0970f
VahanAper <asryanvahandev@gmail.com>
2017-03-16 01:01:41 +0400

    video 25

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
-const API_KEY = 'AIzaSyBA-67_dxDJyAFqQCkdSW7_l66DAR6QRpk'; //eslint-disable-line
+const API_KEY = 'AIzaSyDq0Tx1Ra2azzuVlVhYX0eaUSS-xHja6nY';
 
-const App = () => {
-  return (
-    <div>
-      <p>Ռեա՞կտ</p>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Ռեա՞կտ</p>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
